4dc7cf47b893402e5b25c01b85b0453dfec80286
Farahat <mohamedfarhat5@gmail.com>
2025-12-04 04:10:04 +0200

    User Card & Chairman SLider & Ask

--- src/Views/HomePage/Homepage.tsx
+++ src/Views/HomePage/Homepage.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import CurrentUserCard from "../../webparts/currentUserCard/CurrentUserCard";
+import ChairmanCard from "../../webparts/chairmanCard/ChairmanCard";
+import AskSupport from "../../webparts/askSupport/askSupport";
 
 
 
@@ -7,10 +10,10 @@ export interface IHomePageState {
 }
 
 
-export default class HomePage extends React.Component<any, IHomePageState>{
+export default class HomePage extends React.Component<any, IHomePageState> {
 
 
-    constructor(props:any) {
+    constructor(props: any) {
         super(props);
         this.state = {
             Fullheight: 700,
@@ -18,22 +21,30 @@ export default class HomePage extends React.Component<any, IHomePageState>{
     }
 
     public componentDidMount(): void {
-
-        this.setState({
-            Fullheight: document.getElementsByTagName("body")[0].clientHeight - 267,
-        });
+        console.log(this.props.contextProp)
     }
 
 
     public render() {
         return (
             <>
+                <div className="sectionContent">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-4 col-md-4">
+                                <CurrentUserCard sp={this.props.sp} webUrl={this.props.contextProp.web} />
 
-                <div style={{ minHeight: this.state.Fullheight }}>
-                    <div className="sectionContent">
+                            </div>
+                            <div className="col-8 col-md-8">
+
+                                <ChairmanCard sp={this.props.sp} webUrl={this.props.contextProp.web} listTitle="Leaders" />
+                            </div>
                         </div>
+                        <div className="row">
+                            <AskSupport />
+                        </div>
+                    </div>
                 </div>
-
             </>
         );
 
